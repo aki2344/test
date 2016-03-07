f84d9367451fec42e72965f35fb14df1c7d8d40d
aki2344 <akiyama@suntech.jp>
2016-03-07 19:04:42 +0900

    Update f01.c

--- f01.c
+++ f01.c
@@ -2,10 +2,19 @@
 
 int main(void){
   int a;
-  printf("input >> ");
+  printf("input1 >> ");
   scanf("%d", &a);
   
   printf("out:%d", a);
   
+  printf("input2 >> ");
+  scanf("%d", &a);
+  
+  printf("out:%d", a);
+  
+  printf("input3 >> ");
+  scanf("%d", &a);
+  
+  printf("out:%d", a);
   return 0;
 }
